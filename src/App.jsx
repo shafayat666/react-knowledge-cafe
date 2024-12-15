038ba376b967699bdb6a6686c6b6f45f91290f4a
shafayat666 <shafayatmahin5@gmail.com>
2024-12-15 12:03:52 +0600

    added Blog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Blogs from './components/Header/Blogs/Blogs'
+import Bookmarks from './components/Header/Bookmarks/Bookmarks'
 import Header from './components/Header/Header/Header'
 
 function App() {
-  
+
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className="md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
--- src/components/Header/Header/Header.jsx
+++ src/components/Header/Header/Header.jsx
@@ -1,10 +1,13 @@
+import profile from "../../../assets/images/profile.png";
+
 const Header = () => {
 
   return (
-    <div>
-      <h1 className="text-6xl bg-red-300">
+    <header className="flex justify-between iteems-center p-4 mx-4 border-b-2">
+      <h1 className="text-4xl font-bold">
         Knowledge cafe</h1>
-    </div>
+      <img src={profile}></img>
+    </header>
   );
 };
 
